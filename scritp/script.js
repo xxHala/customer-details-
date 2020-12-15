78084d9eec0064de81e698461b7b6280f1ad19f4
Your Name <hala_azmi@hotmail.com>
2020-12-15 18:23:23 +0200

    hlaf of script

--- scritp/script.js
+++ scritp/script.js
@@ -1,103 +1,140 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+  let personData = {};
+  const mainForm = document.getElementsByClassName('form')[0];
+  const personNameInputs = document.getElementsByClassName('person-name');
+  const formInputs = document.getElementsByClassName('form-input');
+  const personAddress = document.getElementsByClassName('street-add');
+  const personPhone = document.getElementsByClassName('phone-number');
+  const personEmail = document.getElementsByClassName('email');
+  const personChoise = document.getElementsByClassName('Choise');
+  const formHeder = document.getElementsByClassName('form-hed');
+  const formFiled = document.getElementsByClassName('form-field');
+  const er = document.getElementsByClassName('error-name');
 
-    let personData = {};
-    const mainForm = document.getElementsByClassName('form')[0];
-    const personNameInputs = document.getElementsByClassName('person-name');
-    const personAddress = document.getElementsByClassName('street-add');
-    const personPhone = document.getElementsByClassName('phone-number');
-    const personEmail = document.getElementsByClassName('email');
-    const personChoise = document.getElementsByClassName('Choise');
-    const formFiled = document.getElementsByClassName('form-field');
-
-    /**
+  /**
      * Ignore spaces for target input
      * @param {*} event keydown event
      */
-    function preventSpaces(event) {
-        if (event.code === 'Space') {
-            event.preventDefault();
-        }
+  function preventSpaces(event) {
+    if (event.code === 'Space') {
+      event.preventDefault();
     }
-/**
- *  check for email
- * @param {email} user input
- * @return {boolean}
- */
-
-    function ValidateEmail(mail) 
-    {        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-console.log(mail);
-}
-    
-   
-    
-    /**
-     * Checkoption , check for the list if the selected value is 'other' then display
-     * the input box to the user
-     * @param {*}  -selected option from the list
-     * @return {void}
+  }
+  /**
+     * Check for email format
+     * @param {*} event keydown event
      */
-    function Checkoption() {
-            const other = document.querySelector('.other');
-            const option = document.querySelector('.Choise');
-            let value = option.value;
-            if (value == '4') {
-                option[5].text = "other (Please specify..) "
-                other.style.display = 'inline-block';
-            }
-            else {
-                option[5].text = "other"
-                other.style.display = 'none';
-            }
-        
+  function ValidateEmail(event) {
+    const email = personEmail[0].value;
+    const lblError = document.querySelector('.lblError');
+    lblError.innerHTML = '';
+    const expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (!expr.test(email)) {
+      lblError.innerHTML = 'examble@examble.com';
+      event.preventDefault();
+    }
+  }
+
+  // prevent spaces for first name and last name
+  for (let i = 0; i < personNameInputs.length; i++) {
+    personNameInputs[i].addEventListener('keydown', preventSpaces);
+  }
+
+  for (let i = 0; i < personEmail.length; i++) {
+    personEmail[i].addEventListener('keydown', ValidateEmail);
+  }
+
+  /**
+       *  set message error and add class for the background when the validation faild
+       * @param {input}  input input felid to be checked
+       * @param {messge} String message to show when the validation faild
+       * @return {*}
+       */
+
+  function showError(input, messge) {
+    for (let i = 0; i < er.length; i++) {
+      er[i].textContent = messge;
+      er[i].style.cssText = 'visibility: visible';
+    }
+    const child = document.getElementsByClassName('required');
+    for (let i = 0; i < child.length; i++) {
+      child[i].parentElement.classList.add('fieldRequired');
+    }
+  }
+  /** check for name if empty call for error function,
+ * else call for succss
+ * @param{}
+ *
+ */
+  function checkname() {
+    const fName = personNameInputs[0].value.trim();
+    const lName = personNameInputs[1].value.trim();
+    if (!fName && !lName) {
+      showError(fName, 'This felid is required.');
+      showError(lName, 'This felid is required.');
+    } else {
+      personData = {
+        ...personData,
+        firstName: personNameInputs[0].value || '',
+        lastName: personNameInputs[1].value || '',
+
+      };
+      console.log('PersonData', personData);
+    }
+  }
+  document.getElementById('choise-op').addEventListener('change', function () {
+    const style = this.value == 5 ? 'block' : 'none';
+    document.getElementById('other').style.display = style;
+  });
+  /** validate address filed
+ * check for address if empty  call for seterror
+ * @param{}        console.log('focus', );
+*/
+  function checkaddress() {
+    const streetaddress = personAddress[0].value;
+    const cityaddress = personAddress[2].value;
+    const state = personAddress[3].value;
+    const postalcode = personAddress[4].value;
+    if (!streetaddress || !cityaddress
+          || !state || !postalcode) {
+      showError(streetaddress, 'This felid is required.');
+    }
+  }
+
+  function checkphone() {
+    const t = personPhone[0].value;
+    if (t.length < 1) showError(t, 'This felid is required.');
+  }
+
+  function removeError() {
+    for (let i = 0; i < formInputs.length; i++) {
+      formInputs[i].addEventListener('focus', () => {
+        const prent = formInputs[i].closest('.form-field');
+        if (prent.className.includes('fieldRequired')) {
+          prent.classList.remove('fieldRequired');
         }
-
-
-        for (let i = 0; i < personEmail.length; i++) {
-    personEmail[i].addEventListener('keydown',ValidateEmail);
-        }
-    // adding color on focusing
-    for (let i = 0; i < formFiled.length; i++) {
-        formFiled[i].addEventListener('mousedown', changeStyle);
+        er[i].style.cssText = 'visibility: hidden';
+      });
     }
+  }
 
-    // prevent spaces for first name and last name
-    for (let i = 0; i < personNameInputs.length; i++) {
-        personNameInputs[i].addEventListener('keydown', preventSpaces);
-    }
+  removeError();
 
-  
-    function checkInputs() {
-    
-        // trim to remove the whitespaces
-        let usernameValue = personNameInputs[0].value;
-        let userlastnameValue=personNameInputs[1].value;
-        if(usernameValue === ''&& userlastnameValue==='') {
-            setErrorFor(username, 'This felid is required');
-        } else {
-console.log("notempty")        }
-    }
-    
-    function setErrorFor(input, message) {
-        const formControl = input.parentElement;
-        const small = formControl.querySelector('small');
-        formControl.className = 'form-field error';
-        small.innerText = message;
-    }
-   
-        
-    mainForm.addEventListener('submit', e => {
-        e.preventDefault();
-        
-        checkInputs();
-    });
-    /*
+  mainForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkname();
+    checkaddress();
+    checkphone();
+  });
+
+  /*
         mainForm.addEventListener('submit', (e) => {
         e.preventDefault();
         // if (hasErrors()) {
-        //   return
+        //    checkname();
+    checkaddress();
+    checkphone();
         // }
-        checkInputs();
 
         personData = {
             ...personData,
@@ -108,5 +145,5 @@ console.log("notempty")        }
 
         console.log('personData', personData);
         // code here
-    });*/
+    }); */
 });
